f22763d473eb2a8496336fd06830ab0f9b8d83e1
Yesmy Darlery Escobar Escobar <yescobare2@miumg.edu.gt>
2026-09-16 19:11:08 -0600

    Feat(core): agregar la clase de conexion

---
+++ libreria-core/src/main/java/edu/umg/programacion2/proyecto/db/ConexionDB.java
@@ -0,0 +1,14 @@
+package edu.umg.programacion2.proyecto.db;
+ import java.sql.Connection;
+ import java.sql.DriverManager;
+ import java.sql.SQLException;
+public class ConexionDB {
+	private static final String URL = "jdbc:mysql://localhost:3306/libreria_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+}
